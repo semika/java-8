6172d369be0a1ca9d46d0437ed71c732f84ce3ac
semikasi <ssiriwardana@kaleris.com>
2022-01-27 20:33:30 +0530

    modified

--- src/java8/functionalinterface/FunctionalInterfaceMain.java
+++ src/java8/functionalinterface/FunctionalInterfaceMain.java
@@ -1,5 +1,7 @@
 package java8.functionalinterface;
 
+import java.util.function.Function;
+
 public class FunctionalInterfaceMain {
 
     public static void main(String args[]) {
@@ -13,10 +15,20 @@ public class FunctionalInterfaceMain {
         // Implementation two - client two
         Square s2 = (int z) -> z*z*z;
         print(s2, 5);
+
+        //Function
+        StringLengthFunction<String , Integer> fun = (String s) -> s.length();
+        System.out.println(fun.apply("semika"));
+
+        methodWithFunctionAsParameter(fun);
     }
 
     private static void print(Square s, Integer num) {
         //This code behaves like generic
         System.out.println(s.calculateArea(num));
+    }
+
+    private static void methodWithFunctionAsParameter(Function<String, Integer> function) {
+        System.out.println(function.apply("semika"));
     }
 }
